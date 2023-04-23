522402c05a2c2bcdb9767fda77d062b3bcb60e30
GuyNa <34960387+guyna25@users.noreply.github.com>
2023-04-23 23:05:31 +0300

    initial commit
    Upcoming:
    - Add friend card
    - CRUD event with friend
    -

---
+++ client/lib/main.dart
@@ -0,0 +1,36 @@
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'pages/basics_example.dart';
+
+void main() {
+  initializeDateFormatting().then((_) => runApp(MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Friendly - meet your friends',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: StartPage(),
+    );
+  }
+}
+
+class StartPage extends StatefulWidget {
+  @override
+  _StartPageState createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return TableBasicsExample();
+  }
+}
