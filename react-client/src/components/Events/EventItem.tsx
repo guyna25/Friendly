c4cc73deacb10db40c79f24a9761bc096b755b97
GuyNa <34960387+guyna25@users.noreply.github.com>
2023-06-19 17:05:23 +0300

    Added style to eventItem

--- react-client/src/components/Events/EventItem.tsx
+++ react-client/src/components/Events/EventItem.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import EventType from './EventType';
 import {get_full_day_hour} from '../../utils/Utils';
+import styled from 'styled-components';
+
+const StyledEventItem = styled.li`
+  border-color: #050D1F;
+  width: 400px;
+  height: 110px;
+  border: 1px solid black;
+  background-color: #6994EF;
+  padding: 30px;
+  border-radius: 10px;
+  margin-bottom: 5px;
+`;
 
 const EventItem: React.FC<EventType> = (props) => {
-    return (<>
+    return (<StyledEventItem>
         <div>Name: {props.eventTitle}</div>
         <div>Friends: {props.friendNames.join(", ")}</div>
         <div>Date: {`${get_full_day_hour(props.date)} ${props.date.toLocaleDateString()}`}</div>
         <div>Location: {props.location}</div>
         {props.notes !== "" && <div>Notes: {props.notes}</div>}
-    </>
+    </StyledEventItem>
     );
 }
 
--- react-client/src/components/Events/EventsList.tsx
+++ react-client/src/components/Events/EventsList.tsx
@@ -1,15 +1,21 @@
 import EventType from "./EventType";
-import EventItem from "./EventsItem";
+import EventItem from "./EventItem";
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledEventList = styled.ul`
+  padding: 10px;
+`;
 
 const Events : React.FC<{title: string, events:EventType[]}>= (props) => {
-    return <div><h2> {props.title} </h2>
+    return <><h2> {props.title} </h2><StyledEventList>
     {props.events.map(e => <EventItem eventTitle={e.eventTitle} friendNames={e.friendNames}
         location={e.location}
         date={e.date}
         notes={e.notes ?? ''}/>
       )}
-      </div>
+      </StyledEventList>
+      </>
       ;
 }
 
